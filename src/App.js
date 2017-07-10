6aad305e2cf1a298708e67144d37491a921004b8
louisheimel1@gmail.com <louisheimel1@gmail.com>
2017-07-10 17:55:56 -0500

    change http to https

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
 
   componentWillMount() {
     navigator.geolocation.getCurrentPosition(pos => {
-      var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + pos.coords.latitude + '&lon=' + pos.coords.longitude + '&APPID=cf82b1c6672f73b7b74056a6905fb14a'
+      var url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + pos.coords.latitude + '&lon=' + pos.coords.longitude + '&APPID=cf82b1c6672f73b7b74056a6905fb14a'
       axios.get(url)
       .then(data => {
         console.log(data)
